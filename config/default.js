9b38e0c9a5945cd0de017dc2b054acd7bc34d794
tazo-taz <55915855+tazo-taz@users.noreply.github.com>
2022-07-09 20:52:44 +0400

    Update default.js

--- config/default.js
+++ config/default.js
@@ -1,10 +1,10 @@
 module.exports = {
   PORT: 5000,
   MySQL: {
-    host: 'us-cdbr-east-05.cleardb.net',
-    user: 'b0f713354bf10a',
-    password: '4f401bb1',
-    database: 'heroku_60bc0baf6b6fc4e',
+    host: 'us-cdbr-east-05host',
+    user: 'b0f7133user',
+    password: '4f40pass',
+    database: 'heroku_60bc0bdb',
   },
   reactFolder: '../client/public',
   // reactFolder: "./public",
